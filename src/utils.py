c60ebe7b4dd4849ae728a7dcdddffa1ebab4dac9
Robert Poenaru <robert.poenaru@protonmail.ch>
2025-10-27 14:38:48 +0200

    add utils for device management

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+
+
+import torch
+from torch.types import Device
+
+
+def select_optimal_device() -> Device:
+    if torch.cuda.is_available():
+        return torch.device("cuda:0")
+    elif torch.backends.mps.is_available():
+        return torch.device("mps")
+    else:
+        return torch.device("cpu")
