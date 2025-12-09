7e5725f70449bb8016a41f81940b0aa71a806a91
Robert Poenaru <robert.poenaru@protonmail.ch>
2025-12-09 15:28:48 +0200

    add final flop calculation (three methods)

--- src/flops-test/flops.py
+++ src/flops-test/flops.py
@@ -3,6 +3,18 @@ import torch
 from torch.utils.flop_counter import FlopCounterMode
 
 
+class Model(torch.nn.Module):
+    def __init__(self, input_dim: int, output_dim: int) -> None:
+        super(Model, self).__init__()
+        self.linear = torch.nn.Linear(
+            in_features=input_dim, out_features=output_dim)
+
+    def forward(self, x: torch.Tensor):
+        x = x.view(x.shape[0], -1).contiguous()
+        x = self.linear(x)
+        return x
+
+
 if __name__ == "__main__":
     """
     for cifar10
@@ -33,7 +45,7 @@ if __name__ == "__main__":
                     requires_grad=True)
     B = torch.randn((input_dim, output_dim), requires_grad=True)
 
-    use_backward = True
+    use_backward = False
     print('Configs')
     print(f'Using backward pass: {use_backward}')
     print(f'bs = {batch_size} (denoted by M)')
@@ -53,13 +65,27 @@ if __name__ == "__main__":
     print(f'O ( M, N ) -> {O.shape}')
 
     flop_counter = FlopCounterMode(display=False)
-
     with flop_counter:
         if use_backward:
             A.matmul(B).sum().backward()
         else:
             A.matmul(B)
     flops_builtin = flop_counter.get_total_flops()
-
-    print(f'FLOP (builtin): {flops_builtin}')
     assert flops_builtin == flops, ValueError("Inconsistent FLOP estimation")
+
+    print(f'{"="*80}\nSimulating GEMM with `model(x)`')
+    model = Model(input_dim=A.shape[1], output_dim=B.shape[1])
+    flop_counter = FlopCounterMode(display=True, depth=1)
+    A.requires_grad_(False)
+    with flop_counter:
+        model(A)
+        if use_backward:
+            model(A).sum().backward()
+    flops_builtin_model = flop_counter.get_total_flops()
+    assert flops_builtin_model == flops, ValueError(
+        "Inconsistent FLOP estimation")
+
+    print(f'{"="*80}\nFinal results')
+    print(f'FLOP: {flops}')
+    print(f'FLOP (builtin): {flops_builtin}')
+    print(f'FLOP model (builtin): {flops_builtin_model}')
